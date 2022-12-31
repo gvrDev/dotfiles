2cb6ce94976f23901a94792a9d55c2d679df2a9c
gvrDev <guilhermevrodrigues123@gmail.com>
2022-12-31 01:12:14 -0300

    nvim: New remaps

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,12 +1,20 @@
-vim.keymap.set('n',"x", '"_x')
+--remove without messing clipboard
+vim.keymap.set("n", "x", '"_x')
 
-vim.keymap.set('n',"<C-d>", "<C-d>zz")
-vim.keymap.set('n',"<C-u>", "<C-u>zz")
+--move page with cursor in center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set('n',"n", "nzzzv")
-vim.keymap.set('n',"N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
+--copy to system clipboard
 vim.keymap.set("v", "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+--replace all occurrences in file
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+--move selection up and down
+vim.keymap.set("v", "K", [[:m '>+1<CR>gv=gv]])
+vim.keymap.set("v", "J", [[:m '>-2<CR>gv=gv]])
