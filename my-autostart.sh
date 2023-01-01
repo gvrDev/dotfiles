451fe16b1ffcdd6dc6d8f346b41e9e4f4ba6ee38
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-01 14:40:17 -0300

    feat(polybar): Added polybar config and startup bash cmds

--- my-autostart.sh
+++ my-autostart.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/bash
+
 xinput --set-prop 14 'libinput Accel Profile Enabled' 0, 1
 xset r rate 180 90
 
 pactl -- set-sink-volume 0 60%
+
+
+polybar-msg cmd quit
+
+echo "---" | tee -a /tmp/polybar1.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
