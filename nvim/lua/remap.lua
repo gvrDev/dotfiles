8854d3ceeefc593f9ddb2f9f9b0b1237661823c6
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-02 20:48:36 -0300

    refactor(nvim): Created local var to remove prefix vim

--- nvim/lua/remap.lua
+++ nvim/lua/remap.lua
@@ -1,20 +1,22 @@
+local keymap = vim.keymap
+
 --remove without messing clipboard
-vim.keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x')
 
 --move page with cursor in center
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
 
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+keymap.set("n", "n", "nzzzv")
+keymap.set("n", "N", "Nzzzv")
 
 --copy to system clipboard
-vim.keymap.set("v", "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+keymap.set("v", "<leader>y", [["+y]])
+keymap.set("n", "<leader>Y", [["+Y]])
 
 --replace all occurrences in file
-vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
 --move selection up and down
-vim.keymap.set("v", "K", [[:m '>+1<CR>gv=gv]])
-vim.keymap.set("v", "J", [[:m '>-2<CR>gv=gv]])
+keymap.set("v", "K", [[:m '>+1<CR>gv=gv]])
+keymap.set("v", "J", [[:m '>-2<CR>gv=gv]])
