690845ee17643037df0338c638c1b5aa577eb05e
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-02 20:56:07 -0300

    refactor(nvim): Created local variable to remove vim prefix

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,37 +1,38 @@
-vim.opt.guicursor = ""
+local opt = vim.opt
+opt.guicursor = ""
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
 
-vim.opt.smartindent = true
+opt.smartindent = true
 
-vim.opt.wrap = false
+opt.wrap = false
 
-vim.opt.backup = false
-vim.opt.swapfile = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.backup = false
+opt.swapfile = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.hlsearch = false
+opt.incsearch = true
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+opt.colorcolumn = "80"
 
 vim.g.mapleader = " "
-vim.opt.backspace = "start,eol,indent"
+opt.backspace = "start,eol,indent"
 
 vim.o.shell = "/usr/bin/zsh"
 
