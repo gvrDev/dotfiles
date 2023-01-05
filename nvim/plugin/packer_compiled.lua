547438f293757045cec0aae8abf59b2b50ded6ab
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-05 01:22:14 -0300

    package(nvim): packer_compiled

--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -228,6 +228,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/gui/.local/share/nvim/site/pack/packer/start/undotree",
     url = "https://github.com/mbbill/undotree"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/home/gui/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
   }
 }
 
