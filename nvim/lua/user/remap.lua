dad739020bb29ab935f31b256a737b0fd5968de2
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-05 02:49:36 -0300

    fix(nvim): fixed move selection up and down

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -18,5 +18,5 @@ keymap.set("n", "<leader>Y", [["+Y]])
 keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
 --move selection up and down
-keymap.set("v", "K", [[:m '>+1<CR>gv=gv]])
-keymap.set("v", "J", [[:m '<-2<CR>gv=gv]])
+keymap.set("v", "J", [[:m '>+1<CR>gv=gv]])
+keymap.set("v", "K", [[:m '<-2<CR>gv=gv]])
