e66cbc48fbd3a6ca3d32d82067dc33446d0cff5b
gvrDev <guilhermevrodrigues123@gmail.com>
2023-01-08 22:11:45 -0300

    refactor(autostart): bash formatter

--- my-autostart.sh
+++ my-autostart.sh
@@ -5,10 +5,10 @@ xset r rate 180 90
 
 pactl -- set-sink-volume 0 60%
 
-
 polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar1.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log &
+disown
 
 echo "Bars launched..."
