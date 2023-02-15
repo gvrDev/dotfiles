d17992cd45c0a1b4d3a8ca01513fe4b021811b4b
gvrDev <guilhermevrodrigues123@gmail.com>
2023-02-15 13:25:18 -0300

    refactor(nvim): refactored old lsp-zero structure to new one

--- nvim/lua/user/plugins/lsp-zero.lua
+++ nvim/lua/user/plugins/lsp-zero.lua
@@ -2,30 +2,35 @@ local plugin = {
 	setup = function()
 		local lsp = require("lsp-zero")
 
-		lsp.preset("recommended")
+		lsp.preset({
+			name = "recommended",
+			set_lsp_keymaps = false,
+			suggest_lsp_servers = false,
+			sign_icons = false,
+			manage_nvim_cmp = true,
+		})
 
 		lsp.ensure_installed({
 			"tsserver",
-			"sumneko_lua",
 			"angularls",
 			"eslint",
 			"emmet_ls",
+			"rust_analyzer",
+			"lua_ls",
 		})
 
 		lsp.skip_server_setup({ "tsserver" })
 
-		local cmp = require("cmp")
-		local cmp_select = { behavior = cmp.SelectBehavior.Select }
-		local cmp_mappings = lsp.defaults.cmp_mappings({
-			["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-			["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-			["<C-y>"] = cmp.mapping.confirm({ select = true }),
-			["<C-Space>"] = cmp.mapping.complete(),
-		})
+		-- local cmp = require("cmp")
+		-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		-- local cmp_mappings = lsp.defaults.cmp_mappings({
+		-- 	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+		-- 	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+		-- 	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+		-- 	["<C-Space>"] = cmp.mapping.complete(),
+		-- })
 
-		lsp.set_preferences({
-			sign_icons = {},
-		})
+		lsp.nvim_workspace()
 
 		lsp.setup()
 	end,
