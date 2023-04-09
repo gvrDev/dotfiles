b8ea211c928d7503e59f698bed32b8592a683f1d
gvrDev <guilhermevrodrigues123@gmail.com>
2023-04-09 12:36:34 -0300

    refactor(nvim): removed unused plugins

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -2,5 +2,4 @@ return {
 	{ "folke/lazy.nvim", version = "*" },
 	{ "nvim-lua/plenary.nvim", lazy = true },
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	{ "MunifTanjim/nui.nvim", lazy = true },
 }
