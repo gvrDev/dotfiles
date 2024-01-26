7f8f88b4b8cac8f9b5c4a0155b20a7e4cd1c5a36
gvrDev <guilhermevrodrigues123@gmail.com>
2024-01-26 00:51:23 -0300

    update

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+  set -lx SHELL fish
+  keychain --eval --agents ssh --quiet -Q github skey | source
+end
+
+fish_add_path ~/go/bin
+fish_add_path ~/.bun/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
+
+starship init fish | source
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx XDG_CURRENT_DESKTOP "Sway"
