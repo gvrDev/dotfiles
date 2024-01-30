68d5319ab5b5df91f7fdcb3f2f46cb1ad0113e80
gvrDev <guilhermevrodrigues123@gmail.com>
2024-01-30 11:38:57 -0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
   keychain --eval --agents ssh --quiet -Q github skey | source
 end
 
-fish_add_path ~/go/bin
+fish_add_path ~/.go/bin
 fish_add_path ~/.bun/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
